770a94fee63d93872142ddd5dc533cd62004e005
uitanmaciel <uitan.s.maciel@gmail.com>
2024-09-07 14:14:35 -0300

    :white_check_mark: test: Implemented unit test for Reverse String

---
+++ tests/Target.Challenges.Tests/FifthChallengeTest/ReverseStringTest.cs
@@ -0,0 +1,21 @@
+﻿using Target.Challenges.FifthChallenge;
+
+namespace Target.Challenges.Tests.FifthChallengeTest;
+
+[TestClass]
+public class ReverseStringTest
+{
+    [TestMethod]
+    public void ReverseString_WhenCalledWithValidString_ShouldReturnReversedString()
+    {
+        // Arrange
+        var input = "Test";
+        var expected = "tseT";
+
+        // Act
+        var result = ReverseString.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
