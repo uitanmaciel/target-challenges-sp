3aad359920cd5899edbcedb69738e583d2c1eaba
uitanmaciel <uitan.s.maciel@gmail.com>
2024-09-07 12:43:35 -0300

    :sparkles: feat: Implemented Third Challenge - Invoicing

--- src/Target.Challenges/Program.cs
+++ src/Target.Challenges/Program.cs
@@ -1,5 +1,8 @@
-﻿using Target.Challenges.FirstChallenge;
+﻿using System.Globalization;
+using System.Text.Json;
+using Target.Challenges.FirstChallenge;
 using Target.Challenges.SecondChallenge;
+using Target.Challenges.ThirdChallenge;
 
 /*
  Desafio 01 - Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
@@ -8,9 +11,9 @@ Ao final do processamento, qual será o valor da variável SOMA?
 Challenge 01 - Look at the code snippet below: int INDEX = 12, SUM = 0, K = 1; while K < INDEX do { K = K + 1; SUM = SUM + K; } print(SUM);
 At the end of the processing, what will be the value of the SUM variable?
 */
-Console.WriteLine("[Desafio 01] - Calculando a soma dos números de 1 a 13...\n");
-var sum = CalculateSum.ValueSum();
-Console.WriteLine($"SOMA = {sum}\n");
+// Console.WriteLine("[Desafio 01] - Calculando a soma dos números de 1 a 13...\n");
+// var sum = CalculateSum.ValueSum();
+// Console.WriteLine($"SOMA = {sum}\n");
 
 /*
  Desafio 02 - Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos
@@ -25,10 +28,38 @@ Challenge 02 - Given the Fibonacci sequence, where it starts with 0 and 1 and th
  return a message warning if the informed number belongs to the sequence or not.
  */
 
-Console.WriteLine("[Desafio 02] - Verificando se um número pertence a sequência de Fibonacci...\n");
-Console.WriteLine("Digite um número para verificar se ele pertence a sequência de Fibonacci: ");
-var number = int.Parse(Console.ReadLine()!);
-var isFibonacci = FibonacciSequence.IsFibonacci(number);
-Console.WriteLine(isFibonacci 
-  ? "O número informado pertence a sequência de Fibonacci." 
-  : "O número informado não pertence a sequência de Fibonacci.");
+// Console.WriteLine("[Desafio 02] - Verificando se um número pertence a sequência de Fibonacci...\n");
+// Console.WriteLine("Digite um número para verificar se ele pertence a sequência de Fibonacci: ");
+// var number = int.Parse(Console.ReadLine()!);
+// var isFibonacci = FibonacciSequence.IsFibonacci(number);
+// Console.WriteLine(isFibonacci 
+//   ? "O número informado pertence a sequência de Fibonacci." 
+//   : "O número informado não pertence a sequência de Fibonacci.");
+
+/*
+Desafio 03 - Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, que calcule e retorne:
+ - O menor valor de faturamento ocorrido em um dia do mês;
+ - O maior valor de faturamento ocorrido em um dia do mês;
+ - Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+ 
+Challenge 03 - Given an array that stores the daily billing value of a distributor, make a program that calculates and returns:
+ - The lowest billing value that occurred on a day of the month;
+ - The highest billing value that occurred on a day of the month;
+ - Number of days in the month where the daily billing value was higher than the monthly average.
+ */
+
+Console.WriteLine("[Desafio 03] - Calculando o menor, maior faturamento e a média do faturamento mensal...\n");
+var projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+var filePath = Path.Combine(projectDirectory, "ThirdChallenge","data.json");
+var jsonData = File.ReadAllText(filePath);
+var dailyInvoicing = JsonSerializer.Deserialize<IList<Invoicing>>(jsonData);
+var invoicing = new Invoicing(dailyInvoicing);
+var lowestBilling = invoicing.GetLowestBillingValue();
+var highestBilling = invoicing.GetHighestBillingValue();
+var monthlyAverageBilling = invoicing.GetMonthlyAverageBilling();
+var daysAboveMonthlyAverage = invoicing.GetDaysAboveMonthlyAverage();
+
+Console.WriteLine($"Menor faturamento: R$ {lowestBilling.ToString("C", NumberFormatInfo.CurrentInfo)}");
+Console.WriteLine($"Maior faturamento: R$ {highestBilling.ToString("C", NumberFormatInfo.CurrentInfo)}");
+Console.WriteLine($"Média do faturamento mensal: R$ {monthlyAverageBilling}");
+Console.WriteLine($"Número de dias com faturamento acima da média mensal: {daysAboveMonthlyAverage}\n");
---
+++ src/Target.Challenges/ThirdChallenge/Invoicing.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Target.Challenges.ThirdChallenge;
+
+public class Invoicing()
+{
+    [JsonPropertyName("dia")]
+    public int Day { get; set; }
+    
+    [JsonPropertyName("valor")]
+    public decimal Value { get; set; }
+
+    private IList<Invoicing> DailyInvoicing { get; set; } = [];
+    
+    public Invoicing(int day, decimal value) : this()
+    {
+        Day = day;
+        Value = value;
+    }
+    
+    public Invoicing(IList<Invoicing> dailyInvoicing) : this() => DailyInvoicing = dailyInvoicing;
+   
+    /// <summary>
+    /// Calculate the lowest billing (disregarding days with value 0)
+    /// </summary>
+    /// <returns>
+    /// Returns the lowest billing.
+    /// </returns>
+    public decimal GetLowestBillingValue() => DailyInvoicing
+        .Where(x => x.Value > 0)
+        .Min(x => x.Value);
+    
+    
+    /// <summary>
+    /// Calculate the highest billing.
+    /// </summary>
+    /// <returns>
+    /// Returns the highest billing.
+    /// </returns>
+    public decimal GetHighestBillingValue() => DailyInvoicing.Max(x => x.Value);
+    
+    /// <summary>
+    /// Calculate the monthly average billing (disregarding days with value 0)
+    /// </summary>
+    /// <returns>Returns the monthly average billing</returns>
+    public decimal GetMonthlyAverageBilling() => DailyInvoicing
+        .Where(x => x.Value > 0)
+        .Average(x => x.Value);
+    
+    /// <summary>
+    /// Calculate the number of days in the month where the daily billing value was higher than the monthly average
+    /// </summary>
+    /// <returns>Returns the number of days in the month where the daily billing value was higher than the monthly average</returns>
+    public int GetDaysAboveMonthlyAverage() => DailyInvoicing
+        .Count(x => x.Value > GetMonthlyAverageBilling());
+}
